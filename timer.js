a3948445a74f8c5784dcdcf37aa05303129503d4
bakajinsei <cqggff@gmail.com>
2017-03-05 11:36:32 +0800

    Rebuild timer.js with child_process

--- timer.js
+++ timer.js
@@ -1,8 +1,24 @@
 'use strict';
-const app = require('./app');
+const log = require('./common/log');
+const exec = require('child_process').exec;
 const schedule = require('node-schedule');
 
 //定时器任务
 schedule.scheduleJob('0 * * * *', function(){
-    app();
+    exec('node day.js',{
+        'env':{
+            'NODE_ENV':process.env.NODE_ENV||'',
+            'LOG_PATH':process.env.LOG_PATH||''
+        }
+    }, (error, stdout, stderr) => {
+        if (error) {
+            log.error(error);
+        }
+        if(stdout){
+            log.info(stdout);
+        }
+        if(stderr){
+            log.error(stderr);
+        }
+    });
 });
