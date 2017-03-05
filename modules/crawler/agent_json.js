2d3a9b18a277144c6bdcc3e8824ab08ba02e1809
bakajinsei <cqggff@gmail.com>
2017-03-05 12:36:01 +0800

    Change bilibili rule

---
+++ modules/crawler/agent_json.js
@@ -0,0 +1,25 @@
+'use strict';
+const request = require('superagent');
+
+module.exports=function(url,callbackName){
+    return new Promise(function(resolve,reject){
+        request.get(url)
+        .timeout({
+            response: 5000,  // Wait 5 seconds for the server to start sending,
+            deadline: 60000, // but allow 1 minute for the file to finish loading.
+        })
+        .end(function(err,res){
+            if(parseInt(err.statusCode)!==200) return reject(err);
+            try{
+                let backData=err?err.rawResponse:res.text;
+                if(callbackName){
+                    let regRule=new RegExp('^'+callbackName+'\\('+'([\\S\\s]+)\\)(\\;|)$');
+                    backData=backData.replace(regRule,'$1');
+                }
+                resolve(JSON.parse(backData));
+            }catch(parseError){
+                reject(parseError);
+            }
+        });
+    });
+};
--- modules/crawler/bilibili_throw.js
+++ modules/crawler/bilibili_throw.js
@@ -2,7 +2,7 @@
 const tool = require('../../common/tool');
 const log = require('../../common/log');
 const ANIME_GROUP = require('../../enums/anime_group');
-const agentJson = require('./agent_json');
+const agentJSON = require('./agent');
 const groupType='1';
 
 module.exports=function(taskId,url){
@@ -10,16 +10,15 @@ module.exports=function(taskId,url){
         let err=new Error('错误的任务地址,taskId:'+taskId);
         return tool.nextPromise(err);
     }
-    let callbackName='seasonListCallback';
-    let jsonUrl='http://bangumi.bilibili.com/jsonp/seasoninfo/'+url.replace(/^http:\/\/bangumi.bilibili.com\/anime\/(\d+)(|\/)$/i,'$1')+'.ver?callback='+callbackName+'&jsonp=jsonp&_='+new Date().getTime();
-    return agentJson(jsonUrl,callbackName).then(function(result){
+    return agent(url).then(function($){
         let animeList=[];
-        result.result.episodes.sort(function(before,after){
-            return parseInt(before.index) - parseInt(after.index);
-        }).forEach(function(ep){
-            let url=ep.webplay_url;
-            let no=parseInt(ep.index);
-            let title=ep.index_title;
+        let $animeDom=$('.complete-list .v1-bangumi-list-part-child');
+        $animeDom.each(function(){
+            let $this=$(this);
+            let $bangumi=$this.find('.v1-complete-text');
+            let url='http:'+$bangumi.attr('href');
+            let no=$bangumi.find('.text-wrp-num-content').text().replace(/第(\d+)话/g,'$1').trim();
+            let title=$bangumi.find('.text-wrp-title').text().trim();
             if(!ANIME_GROUP.type[groupType].itemRegExp.test(url)){
                 log.warn('错误的分集地址,被抛弃,taskId:'+taskId+',url:'+url);
                 return true;
